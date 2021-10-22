c2a592f90d3dc395e7eb720e5c97d21008a3ddf6
ddlogesh <logeshdina@hypto.in>
2021-10-22 15:38:36 +0530

    Server - printText

--- bar-service-server/src/main/kotlin/com/hypto/hws/services/starter/BarServer.kt
+++ bar-service-server/src/main/kotlin/com/hypto/hws/services/starter/BarServer.kt
@@ -16,12 +16,8 @@
 
 package com.hypto.hws.services.starter
 
-import com.google.protobuf.Timestamp
 import io.grpc.Server
 import io.grpc.ServerBuilder
-import java.time.Instant
-import java.util.*
-
 
 class BarServer(private val port: Int) {
     private val server: Server = ServerBuilder
@@ -50,114 +46,11 @@ class BarServer(private val port: Int) {
     }
 
     private class HyptoBarService : BarGrpcKt.BarCoroutineImplBase() {
-
-        val processedOrders = mutableMapOf<Bill, PaymentStatus>()
-
-        val drinkContainers = mapOf(
-            DrinkType.WHISKY to ContainerType.GLASS,
-            DrinkType.RUM to ContainerType.GLASS,
-            DrinkType.VODKA to ContainerType.GLASS,
-            DrinkType.GIN to ContainerType.GLASS,
-            DrinkType.WINE to ContainerType.CHALICE,
-            DrinkType.BEER to ContainerType.JUG
-        )
-
-        fun Drink.getOrderAmount() : Long = when(this.type) {
-            DrinkType.BEER -> 100
-            DrinkType.VODKA -> 150
-            DrinkType.RUM -> 50
-            DrinkType.WHISKY -> 300
-            DrinkType.GIN -> 200
-            DrinkType.WINE -> 250
-            else -> throw Exception("Bad Request: Unsupported drink type")
-        }
-
-        fun Drink.pour() : Container = run {
-            Container.newBuilder()
-                .setContainerId(UUID.randomUUID().toString())
-                .setDrink(this)
-                .setType(drinkContainers[this.type])
+        override suspend fun printText(request: InputRequest): OutputResponse {
+            println("Input Requested: $request")
+            return OutputResponse.newBuilder()
+                .setText(request.text)
                 .build()
-        }
-
-        fun Long.generateBill() : Bill {
-            val orderId = UUID.randomUUID().toString()
-            val time = Instant.now()
-            val currTimestamp = Timestamp.newBuilder()
-                .setSeconds(time.epochSecond)
-                .setNanos(time.nano)
-                .build()
-
-            val currentBill = Bill.newBuilder()
-                .setOrderId(orderId)
-                .setOrderAmount(this)
-                .setOrderTimestamp(currTimestamp)
-                .build()
-
-            processedOrders[currentBill] = PaymentStatus.PENDING
-            return currentBill
-        }
-
-        override suspend fun orderDrink(request: DrinkRequest): DrinkResponse {
-            println("Requested order:\n$request")
-
-            val container = request.drink.pour()
-            println("Poured drink onto container:\n $container")
-
-            val bill = request.drink.getOrderAmount().generateBill()
-            println("Generated bill:\n$bill")
-
-            println()
-            return DrinkResponse.newBuilder()
-                .setBill(bill)
-                .setContainer(container)
-                .build()
-        }
-
-        override suspend fun orderMultipleDrinks(request: DrinksRequest): DrinksResponse {
-            println("Requested order:\n$request")
-            var orderAmount:Long = 0
-            val containers = request.drinksList.map {
-                orderAmount += it.getOrderAmount()
-                it.pour()
-            }
-
-            return DrinksResponse.newBuilder()
-                .setBill(orderAmount.generateBill())
-                .addAllContainers(containers)
-                .build()
-        }
-
-        override suspend fun payBill(request: PaymentRequest): PaymentResponse {
-            return when(processedOrders[request.bill]) {
-                PaymentStatus.PENDING, PaymentStatus.FAILED -> {
-                    if(request.bill.orderAmount > request.paymentAmount) {
-                        processedOrders[request.bill] = PaymentStatus.FAILED
-                        PaymentResponse.newBuilder()
-                            .setBalanceAmount(request.paymentAmount)
-                            .setStatus(PaymentStatus.FAILED)
-                            .setReason("Bill amount is ${request.bill.orderAmount} but you have only paid ${request.paymentAmount}")
-                            .build()
-                    } else {
-                        processedOrders.remove(request.bill)
-                        PaymentResponse.newBuilder()
-                            .setBalanceAmount(request.paymentAmount - request.bill.orderAmount)
-                            .setStatus(PaymentStatus.PAID)
-                            .setReason("Bill paid successfully!!")
-                            .build()
-                    }
-                }
-                else -> {
-                    if(processedOrders.contains(request.bill)) {
-                        processedOrders.remove(request.bill)
-                    }
-                    PaymentResponse.newBuilder()
-                        .setBalanceAmount(request.paymentAmount)
-                        .setStatus(PaymentStatus.FAILED)
-                        .setReason("Bill with id=${request.bill.orderId} not found! Either it was never created or it was already paid!!")
-                        .build()
-                }
-            }
         }
     }
 }
